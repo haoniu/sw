29738838f85aff6f6001af820643c35085930a79
大山 <woailidashan@gmail.com>
2017-03-28 18:14:39 +0800

    新增LiipImagineBundle

--- sw/app/AppKernel.php
+++ sw/app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-
+            new Liip\ImagineBundle\LiipImagineBundle(),
             new Hn\SwBundle\HnSwBundle(),
         ];
 
