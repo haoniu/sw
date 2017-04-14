0c9b10227910c48b9a05b5bf4cec544769178754
大山 <woailidashan@gmail.com>
2017-04-14 17:57:24 +0800

    打开错误信息

--- sw/web/app.php
+++ sw/web/app.php
@@ -1,12 +1,13 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 Debug::enable();
-$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel('prod', true);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
