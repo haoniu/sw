d875f0ec18c3f0d9415b9fe5ed7f5743bcf072b0
大山 <woailidashan@gmail.com>
2017-04-14 12:00:38 +0800

    新增微信验证

--- sw/src/Hn/SwBundle/Controller/WeChat/IndexController.php
+++ sw/src/Hn/SwBundle/Controller/WeChat/IndexController.php
@@ -3,12 +3,21 @@
 namespace Hn\SwBundle\Controller\WeChat;
 
 use Hn\SwBundle\Controller\WeChat\WxBaseController as TopController;
+use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends TopController
 {
     public function indexAction()
     {
         $this->bootstrap();
+        echo $this->access_token;
         return $this->render('HnSwBundle:WeChat/index:index.html.twig');
+    }
+
+    public function checkAction()
+    {
+        $this->valid();
+
+        return new Response();
     }
 }
--- sw/src/Hn/SwBundle/Controller/WeChat/WxBaseController.php
+++ sw/src/Hn/SwBundle/Controller/WeChat/WxBaseController.php
@@ -94,4 +94,29 @@ class WxBaseController extends BaseController
         }
     }
 
+    //微信接口整合验证进行绑定
+    public function valid() {
+        if ( ! isset( $_GET["echostr"] ) || ! isset( $_GET["signature"] ) || ! isset( $_GET["timestamp"] ) || ! isset( $_GET["nonce"] ) ) {
+            return false;
+        }
+        $echoStr   = $_GET["echostr"];
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce     = $_GET["nonce"];
+        $token     = 'dashan';
+        $tmpArr    = [ $token, $timestamp, $nonce ];
+        sort( $tmpArr, SORT_STRING );
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if ( $tmpStr == $signature ) {
+            //echo $echoStr;
+            return true;
+            exit;
+        } else {
+            return false;
+        }
+    }
+
+
 }
