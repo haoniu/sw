a08f483350c5a1744d84e811bbef3f07a56fd04c
大山 <woailidashan@gmail.com>
2017-05-10 17:38:09 +0800

    新增阿里支付模块

--- sw/src/Hn/SwBundle/Controller/TestController.php
+++ sw/src/Hn/SwBundle/Controller/TestController.php
@@ -3,11 +3,23 @@
 namespace Hn\SwBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class TestController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('', array('name' => $name));
+    }
+
+    public function aliPayAction(Request $request)
+    {
+
+        if($request->getMethod() == 'POST')
+        {
+            $this->get('ali_pay')->pay();
+        }
+
+        return $this->render('HnSwBundle:Default:alipay.html.twig');
     }
 }
